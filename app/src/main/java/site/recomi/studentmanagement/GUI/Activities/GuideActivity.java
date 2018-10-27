2f85ff4b7c3e7aef39744877d902dd07f750cdbd
Krehizi <krehizi@163.com>
2018-10-28 00:05:14 +0800

    test

---
+++ app/src/main/java/site/recomi/studentmanagement/GUI/Activities/GuideActivity.java
@@ -0,0 +1,66 @@
+package site.recomi.studentmanagement.GUI.Activities;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import site.recomi.studentmanagement.GUI.Adapter.MyPagerAdapter;
+import site.recomi.studentmanagement.R;
+
+public class GuideActivity extends AppCompatActivity {
+    List<Integer> bitmaps = new ArrayList<>();
+    ViewPager vp;
+    Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_guide);
+
+        start = findViewById(R.id.startButton);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(GuideActivity.this , MainActivity.class));
+                GuideActivity.this.finish();
+            }
+        });
+
+        vp = findViewById(R.id.vp);
+        bitmaps.add(R.drawable.one);
+        bitmaps.add(R.drawable.two);
+        bitmaps.add(R.drawable.three);
+        vp.setAdapter(new MyPagerAdapter(this ,bitmaps));
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 2){
+                    start.setVisibility(View.VISIBLE);
+                }else {
+                    start.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/site/recomi/studentmanagement/GUI/Activities/WelcomeActivity.java
@@ -0,0 +1,65 @@
+package site.recomi.studentmanagement.GUI.Activities;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import site.recomi.studentmanagement.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+    SharedPreferences.Editor editor;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            getHome();
+            super.handleMessage(msg);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome);
+
+        initializationSettings();
+        if (readIsFirstStart()){
+            editor.putBoolean("isFirstStart" , false);
+            editor.apply();
+
+            startActivity(new Intent(WelcomeActivity.this , GuideActivity.class));
+            finish();
+        }else {
+            //如果不是第一次启动,则设置欢迎界面启动主页面的定时任务
+            handler.sendEmptyMessageDelayed(0,1500);
+        }
+
+    }
+
+    //第一次启动程序初始化一个设置清单
+    private void initializationSettings(){
+        editor = getSharedPreferences("settings" , MODE_PRIVATE).edit();
+        editor.apply();
+    }
+
+    //读取程序是否第一次启动
+    private Boolean readIsFirstStart(){
+        SharedPreferences sharedPreferences = getSharedPreferences("settings" , MODE_PRIVATE);
+        Boolean isFirstStart = sharedPreferences.getBoolean("isFirstStart" , true);
+        return isFirstStart;
+    }
+
+    public void getHome(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        this.finish();
+    }
+}
