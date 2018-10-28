3e0cc4ec1c2cad8ffb9a3c25911f6f9893d60505
Kermi66 <1281540128@qq.com>
2018-10-29 01:19:40 +0800

    fix caps problems for some files

--- app/src/main/java/site/recomi/studentmanagement/utils/NetworkUtil.java
+++ app/src/main/java/site/recomi/studentmanagement/utils/NetworkUtil.java
@@ -1,8 +1,13 @@
 package site.recomi.studentmanagement.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class NetworkUtil {
     /**
@@ -17,5 +22,19 @@ public class NetworkUtil {
         }
         in.close();
         return out.toByteArray();
+    }
+
+    /**
+     * 从网络获取图片，转Bitmap
+     * */
+    public static Bitmap getImage(String url_path) throws IOException {
+        URL url = new URL(url_path);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setConnectTimeout(5000); // Timeout
+        connection.setRequestMethod("GET"); // RequestMethod
+        InputStream inputStream = connection.getInputStream();
+        byte[] data = read(inputStream);
+        inputStream.close();
+        return BitmapFactory.decodeByteArray(data, 0, data.length);
     }
 }
