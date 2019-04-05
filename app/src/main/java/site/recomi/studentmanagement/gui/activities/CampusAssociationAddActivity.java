b96353099db3c556ed117e84744f684043f5ecb3
HS <1638195963@qq.com>
2019-04-05 11:25:57 +0800

    修复add界面

--- app/src/main/java/site/recomi/studentmanagement/gui/activities/CampusAssociationAddActivity.java
+++ app/src/main/java/site/recomi/studentmanagement/gui/activities/CampusAssociationAddActivity.java
@@ -1,11 +1,19 @@
 package site.recomi.studentmanagement.gui.activities;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.gyf.barlibrary.ImmersionBar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,26 +30,36 @@ public class CampusAssociationAddActivity extends AppCompatActivity {
     public RecyclerView rv;
     BaseRecycleViewAdapter<CampusAssociationItem> adapter;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus_association_add);
         ButterKnife.bind(this);
+        getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark2));
 
         List<CampusAssociationItem> lists = new ArrayList<>();
-        lists.add(new CampusAssociationItem("1"));
-        lists.add(new CampusAssociationItem("2"));
-        lists.add(new CampusAssociationItem("3"));
-        lists.add(new CampusAssociationItem("4"));
-        lists.add(new CampusAssociationItem("5"));
+        lists.add(new CampusAssociationItem(R.drawable.bg10 , "学生会" , "校园核心社团"));
+        lists.add(new CampusAssociationItem(R.drawable.bg10 , "吉他社" , "每个人都是歌手"));
+        lists.add(new CampusAssociationItem(R.drawable.bg10 , "学生会" , "校园核心社团"));
+        lists.add(new CampusAssociationItem(R.drawable.bg10 , "学生会" , "校园核心社团"));
+        lists.add(new CampusAssociationItem(R.drawable.bg10 , "学生会" , "校园核心社团"));
+        lists.add(new CampusAssociationItem(R.drawable.bg10 , "学生会" , "校园核心社团"));
+
         adapter = new BaseRecycleViewAdapter<CampusAssociationItem>(this ,lists , R.layout.recycler_view_item_1) {
             @Override
             public void convert(ViewHolder holder, CampusAssociationItem campusAssociationItem) {
-                    holder.setText(R.id.textView2 , campusAssociationItem.getName());
+                    holder.setText(R.id.textView , campusAssociationItem.getName());
+                    holder.setText(R.id.textView2 , campusAssociationItem.getSubTitle());
+                    holder.setImageResource(R.id.imageView2 , campusAssociationItem.getImgSrc());
+
+
+
             }
         };
 
         rv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false));
         rv.setAdapter(adapter);
     }
+
 }
--- app/src/main/java/site/recomi/studentmanagement/other/CampusAssociationItem.java
+++ app/src/main/java/site/recomi/studentmanagement/other/CampusAssociationItem.java
@@ -1,15 +1,39 @@
 package site.recomi.studentmanagement.other;
 
 public class CampusAssociationItem {
+    private int imgSrc;             //头像地址
     private String name;            //社团名
+    private String subTitle;        //子标题
     private Boolean isSelect = false;      //是否选中,默认为不选中
 
-    CampusAssociationItem(String name , Boolean isSelect){
+
+    CampusAssociationItem(int imgSrc , String name ,  String subTitle ,Boolean isSelect){
+        this.imgSrc = imgSrc;
         this.name = name;
+        this.subTitle = subTitle;
         this.isSelect = isSelect;
     }
-    public CampusAssociationItem(String name){
+
+    public int getImgSrc() {
+        return imgSrc;
+    }
+
+    public void setImgSrc(int imgSrc) {
+        this.imgSrc = imgSrc;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public CampusAssociationItem(int imgSrc , String name , String subTitle){
+        this.imgSrc = imgSrc;
         this.name = name;
+        this.subTitle = subTitle;
     }
 
     public String getName() {
